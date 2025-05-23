4946697aeb0f5873f01964e68e485d99e65ad86d
KushNagar2002 <155545416+KushNagar2002@users.noreply.github.com>
2025-05-23 20:20:11 +0530

    Update Problem-2.js

--- Problem-2.js
+++ Problem-2.js
@@ -1,11 +1,15 @@
-function generateOddSeries(a) {
+function generateOddSeriesUpToA(a) {
+  let count = a % 2 === 0 ? a - 1 : a;
+
   let result = [];
-  for (let i = 0; i < a; i++) {
+  for (let i = 0; i < count; i++) {
     result.push(2 * i + 1);
   }
+
   console.log(result.join(", "));
 }
 
-generateOddSeries(1);
-generateOddSeries(2);
-generateOddSeries(5);
+generateOddSeriesUpToA(1);
+generateOddSeriesUpToA(2);
+generateOddSeriesUpToA(4);
+generateOddSeriesUpToA(6);
